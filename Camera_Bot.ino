61501dbfb54b0e15e32da5dbc33d35895a1db349
Garrett Stonis <stonisg@wit.edu>
2021-06-07 22:50:50 -0400

    Finished testing code. Doesn't fry the board!

--- Camera_Bot.ino
+++ Camera_Bot.ino
@@ -1,9 +1,40 @@
-void setup() {
-  // put your setup code here, to run once:
+/* Example sketch to control a 28BYJ-48 stepper motor with ULN2003 driver board and Arduino UNO. More info: https://www.makerguides.com */
+// Include the Arduino Stepper.h library:
+#include <Stepper.h>
 
+
+// Define number of steps per rotation:
+const int stepsPerRevolution = 2048;
+
+
+// Wiring:
+// Pin 8 to IN1 on the ULN2003 driver
+// Pin 9 to IN2 on the ULN2003 driver
+// Pin 10 to IN3 on the ULN2003 driver
+// Pin 11 to IN4 on the ULN2003 driver
+// Create stepper object called 'myStepper', note the pin order:
+Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11);
+
+void setup()
+{
+  // Set the speed to 5 rpm:
+  myStepper.setSpeed(5);
+  
+  // Begin Serial communication at a baud rate of 9600:
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop()
+{
+  byte test = Serial.read();
+  
+  if (test == 97)
+  {
+    myStepper.step(stepsPerRevolution); 
+  }
+  else if (test == 98)
+  {
+    myStepper.step(-stepsPerRevolution);
+  }
 }
